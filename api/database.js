08ce4041c30b6d304b0eff9cbf01bb55eb97846b
marlondelaroch3 <marlondelaroch3@gmail.com>
2022-07-20 20:58:58 -0500

    configurar mongodb y crear ruta para obtener todos los juegos

--- api/database.js
+++ api/database.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
-
+const { config } = require("dotenv");
+config();
+const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_DATABASE } = process.env;
 const connectDB = async () => {
   try {
     const db = await mongoose.connect(
-      "mongodb+srv://marlondelaroch3:1101@loopgame.p84mz.mongodb.net/?retryWrites=true&w=majority"
+      `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@loopgame.p84mz.mongodb.net/${MONGODB_DATABASE}?retryWrites=true&w=majority`
     );
     console.log("DB is connected");
     return db;
