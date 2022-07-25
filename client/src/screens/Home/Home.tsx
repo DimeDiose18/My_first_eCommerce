1059db8e57972af1a6d9ff2d2b70a0d009b703c6
DimeDiose18 <105737605+DimeDiose18@users.noreply.github.com>
2022-07-25 17:19:54 -0500

    add store_nav structure

--- client/src/screens/Home/Home.tsx
+++ client/src/screens/Home/Home.tsx
@@ -1,18 +1,42 @@
 import React from "react";
-import NavBar from "../../component/NavBar/NavBar"
-import styles from "./home.module.css"
-import Drawer from "../../component/Drawer/Drawer"
-
+import NavBar from "../../component/NavBar/NavBar";
+import styles from "./home.module.css";
+import Drawer from "../../component/Drawer/Drawer";
 
 const Home = () => {
-    return (
-        <div className={styles.container}>
-            <div>
-                <NavBar />
-                <Drawer /> 
-            </div>
+  return (
+    <div className={styles.container}>
+      <div>
+        <NavBar />
+        <Drawer />
+      </div>
+      <div className={styles.store_nav}>
+        <div>
+          <ul>
+            <li>
+              <a href="#">Tu tienda</a>
+            </li>
+            <li>
+              <a href="#">Nuevo y destacable</a>
+            </li>
+            <li>
+              <a href="#">Categorías</a>
+            </li>
+            <li>
+              <a href="#">Tienda de puntos</a>
+            </li>
+            <li>
+              <a href="#">Noticias</a>
+            </li>
+            <li>
+              <a href="#">Laboratorios</a>
+            </li>
+          </ul>
         </div>
-    );
-}
+        <input></input>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
