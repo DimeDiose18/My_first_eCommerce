843acf10c936288342ed948671e8662baa677ed0
marlondelaroch3 <marlondelaroch3@gmail.com>
2022-07-27 18:13:59 -0500

    change open drawer to icon burger menu

--- client/src/component/Drawer/Drawer.tsx
+++ client/src/component/Drawer/Drawer.tsx
@@ -1,36 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import Box from "@mui/material/Box";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
-import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-import { styled } from "@mui/material/styles";
+//import { styled } from "@mui/system";
+import "./swipeableDrawer.css";
 
 type Anchor = "left";
 
-const Drawer = () => {
-  const [state, setState] = useState({
-    left: false,
-  });
-
-  const toggleDrawer =
-    (anchor: Anchor, open: boolean) =>
-    (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event &&
-        event.type === "keydown" &&
-        ((event as React.KeyboardEvent).key === "Tab" ||
-          (event as React.KeyboardEvent).key === "Shift")
-      ) {
-        return;
-      }
-
-      setState({ ...state, [anchor]: open });
-    };
-
+type Props = {
+  openDrawer: { left: boolean };
+  toggleDrawer: Function;
+};
+const Drawer = ({ openDrawer, toggleDrawer }: Props) => {
   const list = (anchor: Anchor) => (
     <Box
       sx={{ width: "auto" }}
@@ -63,27 +48,27 @@ const Drawer = () => {
   );
 
   //Aquí pones los estilos
-  const MySwipeableDrawer = styled(SwipeableDrawer)({
+  /* const MySwipeableDrawer = styled(SwipeableDrawer)({
+    paper:{
       backgroundColor: "blue",
+    }
   });
-
+ */
   //
   return (
     <div>
       {(["left"] as const).map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-          <MySwipeableDrawer
+          <SwipeableDrawer
             anchor={anchor}
-            open={state[anchor]}
+            open={openDrawer[anchor]}
             onClose={toggleDrawer(anchor, false)}
             onOpen={toggleDrawer(anchor, true)}
           >
             {list(anchor)}
-          </MySwipeableDrawer>
+          </SwipeableDrawer>
         </React.Fragment>
       ))}
-      ;
     </div>
   );
 };
--- client/src/component/NavBar/NavBar.tsx
+++ client/src/component/NavBar/NavBar.tsx
@@ -1,17 +1,25 @@
-import React from "react";
 import styles from "./navBar.module.css";
 import menuimg from "../../assets/images/menu1.png";
 import logoimg from "../../assets/images/logoloopcompleto.svg";
 
-const NavBar = () => {
+type Props = {
+  toggleDrawer: Function;
+  openDrawer: { left: boolean };
+};
+const NavBar = ({ toggleDrawer, openDrawer }: Props) => {
   return (
     <nav>
       <div>
-        <img className={styles.menu} src={menuimg}></img>
+        <img
+          onClick={toggleDrawer("left", !openDrawer.left)}
+          className={styles.menu}
+          src={menuimg}
+          alt="img"
+        ></img>
       </div>
 
       <div className={styles.navbar_logo}>
-        <img className={styles.img2} src={logoimg}></img>
+        <img className={styles.img2} src={logoimg} alt="img"></img>
       </div>
     </nav>
   );
